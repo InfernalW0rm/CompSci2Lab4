14d2fa75f1686979e41d532e0fccc58361d88994
Andrew Diggs <andrewrdiggs@gmail.com>
2024-08-06 11:58:28 -0500

    created the header file for color and also formatted the picture.txt file

--- CompSci2Lab4/Color.h
+++ CompSci2Lab4/Color.h
@@ -1,5 +1,18 @@
-#pragma once
-class Color
-{
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <iostream>
+using namespace std;
+
+class Color {
+public:
+    int red, green, blue;
+
+    Color(int r, int g, int b);
+
+    static const Color RED, YELLOW, BLUE, GREEN, PURPLE, ORANGE, BLACK, WHITE, GRAY;
+
+    friend ostream& operator<<(ostream& os, const Color& color);
 };
 
+#endif
