3a991a0013e5241f04301067d743d8233be64268
Andrew Diggs <andrewrdiggs@gmail.com>
2024-08-08 12:15:23 -0500

    finished the types of exceptions

--- CompSci2Lab4/Exceptions.cpp
+++ CompSci2Lab4/Exceptions.cpp
@@ -6,18 +6,44 @@
 
 using namespace std;
 
-class FileNotFoundException : public exception {
+class FileNotFoundException : public exception 
+{
 public:
-    const char* what() const noexcept override {
+    const char* what() const noexcept override 
+    {
         return "File not found.";
     }
 };
 
-class ColorFileFormatException : public exception {
+class ColorFileFormatException : public exception 
+{
+
 public:
-    const char* what() const noexcept override {
+    const char* what() const noexcept override 
+    {
         return "Color file format is incorrect.";
     }
 };
 
+class CoordinateOutOfRangeException : public exception 
+{
+public:
+    const char* what() const noexcept override 
+    {
+        return "Coordinate out of range.";
+    }
+};
+
+class ColorVectorOutOfRangeException : public exception 
+{
+    string message;
+
+public:
+    ColorVectorOutOfRangeException(const string& msg) : message(msg) {}
+
+    const char* what() const noexcept override 
+    {
+        return message.c_str();
+    }
+};
 #endif
